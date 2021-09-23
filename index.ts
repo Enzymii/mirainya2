7f42c17d35b4815a8f68a3a2ca61c911b1369c1b
enzymii <enzymiix@gmail.com>
2021-09-23 15:59:28 +0800

    🎨  change export format

--- index.ts
+++ index.ts
@@ -1,10 +1,8 @@
 import Bot from './src/bot';
-export default Bot;
-
 import MakeMsg from './src/utils/message';
 import Logger from './src/utils/log';
 
-export { MakeMsg, Logger };
+export { Bot, MakeMsg, Logger };
 
 export * from './src/types/event';
 export * from './src/types/message';
